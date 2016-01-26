ce594376c5e73ecb7138f9c491bc716c2f046f0b
Melvin Daytree <melvin.daytree@uconn.edu>
2016-01-25 20:12:40 -0500

    all set

--- variables.js
+++ variables.js
@@ -1,3 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<script>
 var num1 === 21
 var num2 === 54
 var name === "Melvin"
@@ -6,3 +15,8 @@ console.log(num1);
 console.log(num2);
 console.log(num1 + num2)
 console.log("My name is " + name)
+</script>
+</head>
+<body>
+</body>
+</html>
